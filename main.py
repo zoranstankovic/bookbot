cb6ec713119fe327ebf15dc79304ade49e891c7b
zoranstankovic <6569042+zoranstankovic@users.noreply.github.com>
2025-11-23 13:21:34 +0100

    add argument parsing to main.py for specifying a book file path via command line

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
+
 from stats import count_words, get_char_count, sort_characters
 
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     total_words = count_words(book_text)
     num_chars = get_char_count(book_text)
